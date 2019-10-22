a1d1505cf12f501ccb717ecc0edb4eef999cb06d
Luong Quang Manh <luongquangmanh85@gmail.com>
2019-10-22 07:02:19 +0700

    Remove bloated boilerplate

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  el: '#app',
   render: (h) => h(App)
-}).$mount('#app')
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import VueRouter from 'vue-router'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: () => import('../views/Home.vue')
     },
     {
       path: '/about',
@@ -19,7 +18,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
   ]
 })
