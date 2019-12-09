5c04b5cb23574b2b2b16e1625f6b92c85e2ab403
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2019-12-09 23:53:59 +0700

    Use Nuxt.js instead

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <h1 class="title">Steganopy</h1>
+    <h2 class="subtitle">
+      Steganography with serverless architecture.
+    </h2>
+  </div>
+</template>
--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app">
-    <h1>This is Home page</h1>
-  </div>
-</template>
-
-<style lang="scss">
-@import 'assets/styles.scss';
-</style>
