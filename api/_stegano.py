6298d3e42b3a39c4fc4efda50dbeea742d577fb2
Lương Quang Mạnh <luongquangmanh85@gmail.com>
2019-12-09 18:38:29 +0700

    Fix things

--- api/_stegano.py
+++ api/_stegano.py
@@ -5,15 +5,12 @@ import struct
 import numpy as np
 
 
-MAX_LSB = 2  # use up to 2 LSBs per channel per pixel
-
-
 class SteganopyException(Exception):
     pass
 
 
 def conceal(img_buffer: bytes, payload_buffer: bytes) -> bytes:
-    """Embed a payload into an image, and return the output image."""
+    """Embed data into an image, and return the output image."""
 
     img = Image.open(BytesIO(img_buffer))
     width, height = img.size
@@ -21,21 +18,16 @@ def conceal(img_buffer: bytes, payload_buffer: bytes) -> bytes:
     if not _validate_payload_size(width, height, len(payload_buffer)):
         raise SteganopyException("Cannot process. The payload is too large.")
 
-    # the output image will be encoded as PNG in the end
-    img = img.convert("RGBA")
-    img_arr = np.array(img)
-    payload_bits = _decompose(payload_buffer)
+    img_arr = np.array(img.convert("RGBA"))
 
+    payload_bits = _decompose(payload_buffer)
+    bits_len = len(payload_bits)
     i = 0  # index of the payload bits
-    for nth, y, x in np.ndindex(MAX_LSB, height, width):
-        if i >= len(payload_bits):
+    for color in np.nditer(img_arr, op_flags=["readwrite"]):
+        color[...] = _set_bit(color, payload_bits[i])
+        i += 1
+        if i >= bits_len:
             break
-        r, g, b, a = img_arr[y, x]
-        r = _set_bit(r, payload_bits[i], nth)
-        g = _set_bit(g, payload_bits[i + 1], nth)
-        b = _set_bit(b, payload_bits[i + 2], nth)
-        img_arr[y, x] = r, g, b, a
-        i += 3
     output_img = Image.fromarray(img_arr)
     output_buffer = BytesIO()
     output_img.save(output_buffer, format="PNG")
@@ -43,31 +35,34 @@ def conceal(img_buffer: bytes, payload_buffer: bytes) -> bytes:
 
 
 def reveal(img_buffer: bytes) -> bytes:
+    """Extract secret data from an image."""
+
     img = Image.open(BytesIO(img_buffer))
-    width, height = img.size
-    img = img.convert("RGBA")
-    img_arr = np.asarray(img)
-    payload_bits = np.empty(MAX_LSB * 3 * width * height, np.uint8)
+    img_arr = np.asarray(img.convert("RGBA")).ravel()
 
-    i = 0  # index of the payload bits
-    for nth, y, x in np.ndindex(MAX_LSB, height, width):
-        r, g, b, _ = img_arr[y, x]
-        payload_bits[i] = _get_bit(r, nth)
-        payload_bits[i + 1] = _get_bit(g, nth)
-        payload_bits[i + 2] = _get_bit(b, nth)
-        i += 3
+    payload_size_bits = np.empty(32, np.uint8)
+    i = 0
+    for color in np.nditer(img_arr[:32]):
+        payload_size_bits[i] = _get_bit(color)
+        i += 1
+    payload_size = _assemble_int(payload_size_bits)  # bytes
+
+    bits_len = payload_size * 8  # bits
+    payload_bits = np.empty(bits_len, np.uint8)
+    i = 0
+    for color in np.nditer(img_arr[32:]):
+        payload_bits[i] = _get_bit(color)
+        i += 1
+        if i >= bits_len:
+            break
     return _assemble(payload_bits)
-
-
-def analyse(img_buffer: bytes):
-    pass
 
 
 def _validate_payload_size(img_w: int, img_h: int, payload_size: int) -> bool:
     """Return True if the payload is small enough, relative to the cover image."""
 
-    # conceal data in 3 channels
-    max_size = MAX_LSB / 8 * (3 * (img_w * img_h))  # bytes
+    # conceal data in 4-channel pixels, 1 bit per channel
+    max_size = img_w * img_h // 2  # bytes, (4 * img_w * img_h // 8) actually
     return payload_size <= max_size - 4  # need 4 bytes to store the payload size
 
 
@@ -78,31 +73,34 @@ def _decompose(data: bytes) -> list:
     byte_arr = np.empty(4 + size, np.uint8)
     byte_arr[:4] = tuple(struct.pack("i", size))
     byte_arr[4:] = tuple(data)
-
-    bit_arr = np.unpackbits(byte_arr)
-    r = len(bit_arr) % 3
-    if not r:
-        return bit_arr
-    return np.concatenate((bit_arr, np.zeros(3 - r, np.uint8)))
+    return np.unpackbits(byte_arr)
 
 
-def _assemble(data: list) -> bytes:
-    byte_arr = np.packbits(data)
-    size = struct.unpack("i", byte_arr[:4])[0]
-    return byte_arr[4 : size + 4].tobytes()
+def _assemble_int(bit_arr: np.ndarray) -> int:
+    """Assemble a list of 32 bits to a 4-byte integer."""
+
+    byte_arr = np.packbits(bit_arr)
+    return struct.unpack("i", byte_arr)[0]
 
 
-def _set_bit(color: int, bit: int, nth: int = 0):
+def _assemble(bit_arr: np.ndarray) -> bytes:
+    """Assemble a list of bits to binary data."""
+
+    byte_arr = np.packbits(bit_arr)
+    return byte_arr.tobytes()
+
+
+def _set_bit(color: np.ndarray, bit: int, nth: int = 0):
     """Set the n-th bit of a color and return the masked one."""
 
     mask = 1 << nth
-    color &= ~mask
+    color = np.bitwise_and(color, ~mask)
     if bit:
-        color |= mask
+        color = np.bitwise_or(color, mask)
     return color
 
 
-def _get_bit(color: int, nth: int = 0) -> int:
+def _get_bit(color: np.ndarray, nth: int = 0) -> int:
     """Get the n-th bit of a color."""
 
     mask = 1 << nth
