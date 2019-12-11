f2acb132fb196ca865ea6c4bed988485f196743d
Đức Nghĩa <nghiaduy0@gmail.com>
2019-12-12 00:16:36 +0700

    Update project (#3)

--- pages/detect.vue
+++ pages/detect.vue
@@ -2,5 +2,80 @@
   <div class="container">
     <h3 class="title">Data detecting</h3>
     <h5 class="subtitle">Check if an image contains any secret data.</h5>
+    <uploadForm
+      title="Upload image to detect:"
+      acceptFile="image/*"
+      :fileName="imageFileName"
+      @input="changeImageFile"
+    />
+    <div class="columns">
+      <div class="control column">
+        <button class="button is-info" @click="check">Check</button>
+      </div>
+    </div>
+    <div v-if="responseDataUrl">
+      <div v-if="hasData" class="has-text-success is-centered">
+        <span class="icon">
+          <ion-icon name="checkmark"></ion-icon>
+        </span>
+        <span>This image has data inside!</span>
+      </div>
+      <div v-else class="has-text-danger">
+        <span class="icon">
+          <ion-icon name="close"></ion-icon>
+        </span>
+        <span>This image doesn't have data inside!</span>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+import uploadForm from '../components/upload-form'
+
+export default {
+  name: 'Check',
+  components: {
+    uploadForm
+  },
+  data() {
+    return {
+      imageFileName: 'No image choosen...',
+      imageFile: null,
+      responseDataUrl: '',
+      hasData: Boolean
+    }
+  },
+  methods: {
+    changeImageFile(event) {
+      this.imageFile = event
+      this.imageFileName = event[0].name
+    },
+    check() {
+      const formData = new FormData()
+      formData.append('files', this.imageFile[0])
+      axios
+        .post('/api/detect', formData, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        })
+        .then((response) => {
+          if (response.data.result !== undefined) {
+            this.hasData = response.data.result
+          } else {
+            console.log(response.data.error)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style>
+template {
+  margin: auto;
+}
+</style>
--- pages/reveal.vue
+++ pages/reveal.vue
@@ -1,37 +1,32 @@
 <template>
-  <div class="is-centered">
-    <div class="card">
-      <header class="card-header">
-        <p class="card-header-title is-centered">DECODE</p>
-      </header>
-      <div class="card-content">
-        <div class="content">
-          <upload-form
-            title="Upload image to decode:"
-            accept-file="image/*"
-            :file-name="imageFileName"
-            @input="changeImageFile"
-          />
-          <div class="columns">
-            <div class="control column">
-              <button class="button is-dark" @click="decode">Decode</button>
-            </div>
-          </div>
-          <div v-if="responseDataUrl">
-            <div class="field has-addons">
-              <div class="control">
-                <input class="input" type="text" :value="responseDataUrl" readonly />
-              </div>
-              <div class="control">
-                <a class="button is-info">
-                  <span class="file-icon">
-                    <ion-icon name="cloud-download"></ion-icon>
-                  </span>
-                  <span class="file-label" @click="dowloadResponseData">Download</span>
-                </a>
-              </div>
-            </div>
-          </div>
+  <div class="container">
+    <h3 class="title">Data revealing</h3>
+    <h5 class="subtitle">Extract data from an image.</h5>
+    <upload-form
+      title="Upload image to decode:"
+      accept-file="image/*"
+      :file-name="imageFileName"
+      @input="changeImageFile"
+    />
+    <div class="columns">
+      <div class="control column">
+        <button class="button is-dark" @click="decode">Decode</button>
+      </div>
+    </div>
+    <div v-if="responseDataUrl">
+      <div class="field has-addons">
+        <div class="control">
+          <input class="input" type="text" :value="responseDataUrl" readonly />
+        </div>
+        <div class="control">
+          <a class="button is-info">
+            <span class="file-icon">
+              <ion-icon name="cloud-download"></ion-icon>
+            </span>
+            <span class="file-label">
+              <a :href="responseDataUrl" target="_blank">Download</a>
+            </span>
+          </a>
         </div>
       </div>
     </div>
@@ -39,6 +34,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import UploadForm from '../components/upload-form'
 
 export default {
@@ -49,7 +45,7 @@ export default {
   data() {
     return {
       imageFileName: 'No image choosen...',
-      imageFile: File,
+      imageFile: null,
       responseDataUrl: ''
     }
   },
@@ -58,13 +54,28 @@ export default {
       this.imageFile = event
       this.imageFileName = event[0].name
     },
-    decode() {},
-    dowloadResponseData() {}
+    decode() {
+      axios
+        .post('/api/reveal')
+        .then((response) => {
+          if (response.data.url !== undefined) {
+            this.responseDataUrl = response.data.url
+          } else {
+            console.log(response.data.error)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
 
 <style>
+a {
+  color: aliceblue;
+}
 @media only screen and (min-width: 768px) {
   .card {
     max-width: 40%;
